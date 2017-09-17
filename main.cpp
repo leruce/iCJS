89a767019cc618a0186692472dee3622f9eac3f1
chuck9779 <32044043+chuck9779@users.noreply.github.com>
2017-09-17 18:41:17 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello This will be our First commit for our main function" << endl;
     cout << "Hello. Practicing doing my First branch and commit" << endl;
+	cout << "This is my first commit." // added by CG
     return 0;
 }
