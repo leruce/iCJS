9bd02958eba9412948297c0973d0aaff7ca4e341
cavanderbilt <32026253+cavanderbilt@users.noreply.github.com>
2017-09-30 17:25:05 -0500

    Add files via upload

---
+++ cProlog/c_src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main() {
+    cout << "Hello This will be our First commit for our main function" << endl;
+    cout << "Hello. Practicing doing my First branch and commit" << endl;
+    cout << "This is my first commit."; // added by CG
+    return 0;
+}
