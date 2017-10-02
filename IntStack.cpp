86e3450c4ee520db4935dcbf0f53a296eb2662ae
cavanderbilt <32026253+cavanderbilt@users.noreply.github.com>
2017-10-02 10:54:55 -0500

    Add files via upload
    Found some more errors in IntMap: intersect0
    I commented out the original line and replaced it with my correction.
    
    For both IntStack and IntMap, CLion gave me errors about the static variables
    error: non-const static data member must be initialized out of line
    I moved them to outside of the class and the error went away.
    
    I was getting weird errors on IntMap.cpp: delete.
    I think delete is a reserved word. Changed the function name to toDelete
    Again I commented out the original line, copied it, and made the changes there.
    I created .h files for them because IntMap needed to be pointed to IntStack so I added #include "IntStack.h" . This fixed the error.
    However, even though I included #include "IntStack.h" in my IntList.h, it's giving me an unknown type name "IntStack". I don't know why.
    
    My files are in cProlog/c_src

--- IntStack.cpp
+++ IntStack.cpp
@@ -1,94 +1,98 @@
 #include <iostream>
 #include <vector>
+#include "IntStack.h"
+
 
 using namespace std;
+
+static int FREE_KEY = 0;
+static int NO_VALUE = 0;
 
 
 class IntStack {
 
 private:
-  vector<int> stack;
+    vector<int> stack;
 
-  int top;
+    int top;
+    int csize = 16;
 
-  int csize = 16;
-
-  int MINSIZE = 1 << 15;
+    int MINSIZE = 1 << 15;
 
 public:
-  IntStack() {
-    init(csize);
-  }
-  IntStack(int size) {
-    init(size);
-  }
-  void init(int size) {
-    stack.reserve(size);
-    clear();
-  }
-
-  int getTop() {
-    return top;
-  }
-  int setTop(int top) {
-    return this->top = top;
-  }
-
-  void clear() {
-    top = -1;
-  }
-
-  bool isEmpty() {
-    return top < 0;
-  }
-  void push (int i) {
-    if (++top >= stack.size()) {
-      expand();
+    IntStack() {
+        init(csize);
     }
-    stack[top] = i;
-  }
-  int pop() {
-    int r = stack[top--];
-    strink();
-    return r;
-  }
-
-  int get(int i) {
-    return stack[i];
-  }
-  void set(int i, int val) {
-    stack[i] = val;
-  }
-
-  int size() {
-    return top + 1;
-  }
-
-  void expand() {
-    stack.resize(stack.size() * 2);
-  }
-
-  void shrink() {
-    int l = stack.size();
-    if ( l <= MINSIZE || top << 2 >= 1) {
-      return;
+    IntStack(int size) {
+        init(size);
     }
-    l = 1 + (top << 1);
-    if (top < MINSIZE) {
-      l = MINSIZE;
+    void init(int size) {
+        stack.reserve(size);
+        clear();
     }
-    stack.resize(l);
-  }
 
-  void reverse() {
-    int l = size();
-    int h = l >> 1;
-    for (int i = 0; i < h; i++) {
-      int temp = stack[i];
-      stack[i] = stack[ l-i -1];
-      stack[l-i-1] = temp;
+    int getTop() {
+        return top;
     }
-  }
+    int setTop(int top) {
+        return this->top = top;
+    }
 
-  
-}
+    void clear() {
+        top = -1;
+    }
+
+    bool isEmpty() {
+        return top < 0;
+    }
+    void push (int i) {
+        if (++top >= stack.size()) {
+            expand();
+        }
+        stack[top] = i;
+    }
+    int pop() {
+        int r = stack[top--];
+        shrink();
+        return r;
+    }
+
+    int get(int i) {
+        return stack[i];
+    }
+    void set(int i, int val) {
+        stack[i] = val;
+    }
+
+    int size() {
+        return top + 1;
+    }
+
+    void expand() {
+        stack.resize(stack.size() * 2);
+    }
+
+    void shrink() {
+        int l = stack.size();
+        if ( l <= MINSIZE || top << 2 >= 1) {
+            return;
+        }
+        l = 1 + (top << 1);
+        if (top < MINSIZE) {
+            l = MINSIZE;
+        }
+        stack.resize(l);
+    }
+
+    void reverse() {
+        int l = size();
+        int h = l >> 1;
+        for (int i = 0; i < h; i++) {
+            int temp = stack[i];
+            stack[i] = stack[ l-i -1];
+            stack[l-i-1] = temp;
+        }
+    }
+
+
+};
---
+++ IntStack.h
@@ -0,0 +1,12 @@
+//
+// Created by COLLEEN VANDERBILT on 10/1/17.
+//
+
+#ifndef IPROLOG_INTSTACK_H
+#define IPROLOG_INTSTACK_H
+
+
+
+
+
+#endif //IPROLOG_INTSTACK_H
