944ccda9bc5a79a097d69aa3df1178d9be830372
Garrett Addington <garrettaddington@my.unt.edu>
2017-10-07 02:32:10 -0500

    Added a 2nd Verison of Stack file. Check over then replace it
    2nd Verison of Stack file

--- cProlog/c_src/IntStack.h
+++ cProlog/c_src/IntStack.h
@@ -5,7 +5,33 @@
 #ifndef IPROLOG_INTSTACK_H
 #define IPROLOG_INTSTACK_H
 
+#include <iostream>
+#include <vector>
 
+
+class IntStack {
+public:
+  IntStack();
+  IntStack(int size);
+  void init(int size);
+  int getTop();
+  int setTop();
+  void clear();
+  bool isEmpty();
+  void push(int i);
+  int pop();
+  int get(int i);
+  void set(int i, int val);
+  int size();
+  void expand();
+  void shrink();
+  void reverse();
+
+
+private:
+  vector<int> stack;
+  int top;
+};
 
 
 
