1ccd0b172bcb78a1a78e6cb476fab2370bb9aa8f
kaashis <32060119+kaashis@users.noreply.github.com>
2017-11-13 15:23:49 -0600

    Add files via upload

---
+++ testProlog/JavaTest/TestIntList.java
@@ -0,0 +1,16 @@
+
+public class TestIntList {
+
+	public static void main(String[] args) {
+		
+		
+		IntList L1 = new IntList (1);
+		IntList L2 = new IntList (2,L1);
+		IntList L3 = new IntList (3,L2);
+		
+		System.out.println(L3);
+		
+	
+
+    }
+}
