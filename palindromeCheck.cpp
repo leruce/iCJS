2ecc93dc241dd2314ec47702548dd92c8e6bf6a5
Khorrom <mdkhorromkhan@my.unt.edu>
2017-11-16 13:08:30 -0400

    palindromeCheck

--- palindromeCheck.cpp
+++ palindromeCheck.cpp
@@ -4,31 +4,24 @@
 using namespace std;
 
 int main(){
+    char inFile[15];
+
+    ifstream Input;
+    cout<<"Enter the filename: ";
+    cin>>inFile;
+    Input.open(inFile);
 
     string str;
     int i, length;
     int flag = 0;
 
-    cout << "Enter a string: "; cin >> str;
+    cout<<"Following words are palindrome: "<<endl;
+    while(getline(Input,str)){
 
-    length = str.length();
-
-    for(i=0;i < length ;i++){
-        if(str[i] != str[length-i-1]){
-            flag = 1;
-            break;
-	   }
-	}
-
-    if (flag) {
-        cout << str << " is not a palindrome" << endl;
-    }
-    else {
-        cout << str << " is a palindrome" << endl;
-    }
-
+            if (str == string(str.rbegin(), str.rend())) {
+            cout << str<<endl;
+        }
+}
     return 0;
 
 }
-
-
