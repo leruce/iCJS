0d06bf84a8ac2f47e15ade36a564fce4b32c5c07
leruce <garrettaddington@my.unt.edu>
2017-11-16 10:34:02 -0600

    Update main.cpp
    Finished the Code itself. Need to add in the file needed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 
 
-int main() {
-    cout << "First Commit in the Question 2." << endl;
+int main(int argc, char **argv) {
+    //cout << "First Commit in the Question 2." << endl;
+    string inFile;
+    if (argc = 2 ) {
+        inFile = argv[1];   
+    }
+    else {
+    
+    }
+    ifstream Input;
+    Input.open(inFile);
+    string str;
+    while (getline(Input,str)) {
+        //Each Line have 1 word.
+        if (str == string(str.rbegin(), str.rend())) {
+            cout << input << "is a palindrome!" << endl;   
+        }
+    }
+    
     return 0;
 }
